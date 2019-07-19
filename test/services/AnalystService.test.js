9fb7237e1f02901021e96a43efa6214387886410
Denis Rudnei de Souza <denisrudneidesouza@gmail.com>
2019-07-19 17:39:34 -0300

    Set timeout in tests to 0

--- test/services/AnalystService.test.js
+++ test/services/AnalystService.test.js
@@ -1,18 +1,22 @@
 const AnalystService = require('../../server/services/AnalystService')
 const AnalystSeed = require('../seeds/AnalystSeed')
 const analyst = AnalystSeed.seed(1)[0]
-it('Get analysts', async () => {
-  await AnalystService.getAnalysts()
-})
 
-it('Create new analyst', async () => {
-  await AnalystService.create(analyst)
-})
+describe('Analyst', function() {
+  this.timeout(0)
+  it('Get analysts', async () => {
+    await AnalystService.getAnalysts()
+  })
 
-it('Get analyst in admin view', async () => {
-  await AnalystService.getConfigAnalysts()
-})
+  it('Create new analyst', async () => {
+    await AnalystService.create(analyst)
+  })
 
-it('Remove user image', async () => {
-  await AnalystService.removeImage(analyst._id)
+  it('Get analyst in admin view', async () => {
+    await AnalystService.getConfigAnalysts()
+  })
+
+  it('Remove user image', async () => {
+    await AnalystService.removeImage(analyst._id)
+  })
 })
--- test/services/AuthService.test.js
+++ test/services/AuthService.test.js
@@ -4,15 +4,18 @@ const AuthService = require('../../server/services/AuthService')
 const password = faker.internet.password()
 const email = faker.internet.email()
 
-it('Register new user', async () => {
-  const newUser = {
-    name: 'teste',
-    email: email,
-    password: password
-  }
-  await AuthService.register(newUser)
-})
+describe('Auth', function() {
+  this.timeout(0)
+  it('Register new user', async () => {
+    const newUser = {
+      name: 'teste',
+      email: email,
+      password: password
+    }
+    await AuthService.register(newUser)
+  })
 
-it('Login', async () => {
-  await AuthService.login(email, password)
+  it('Login', async () => {
+    await AuthService.login(email, password)
+  })
 })
