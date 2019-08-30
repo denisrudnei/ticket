96d63368642744180c2b7e36f6d4fd3fecda2eec
Denis Rudnei de Souza <denisrudneidesouza@gmail.com>
2019-08-30 19:17:46 -0300

    Added more tests

--- test/services/StatusService.test.js
+++ test/services/StatusService.test.js
@@ -1,4 +1,5 @@
 const StatusService = require('../../server/services/ticket/StatusService')
+const Status = require('../../server/models/ticket/Status')
 
 describe('Status', () => {
   it('Get status', async () => {
@@ -10,5 +11,15 @@ describe('Status', () => {
       name: 'test'
     }
     await StatusService.create(status)
+  })
+  it('Get one status', async () => {
+    const status = await Status.findOne().exec()
+    await StatusService.getOne(status._id)
+  })
+
+  it('Create new status', async () => {
+    const status = await Status.findOne().exec()
+    status.name = 'updated name'
+    await StatusService.edit(status._id, status)
   })
 })
