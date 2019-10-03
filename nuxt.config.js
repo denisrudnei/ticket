79230cba925e2c2435727dfa217b319d600b071d
Denis Rudnei de Souza <denisrudneidesouza@gmail.com>
2019-10-03 08:53:35 -0300

    Fixed error in nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -73,13 +73,6 @@ module.exports = {
     customVariables: [],
     theme: {
       dark: false,
-      primary: '#673ab7',
-      accent: colors.grey.darken3,
-      secondary: colors.amber.darken3,
-      info: colors.teal.lighten1,
-      warning: colors.amber.base,
-      error: colors.deepOrange.accent4,
-      success: colors.green.accent3
       themes: {
         light: {
           primary: '#673ab7',
