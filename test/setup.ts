aeede3db58b934d98ae38baea413a5c70fb12367
Denis Rudnei de Souza <denisrudneidesouza@gmail.com>
2020-03-12 14:14:13 +0000

    Added role check in tests

--- test/setup.ts
+++ test/setup.ts
@@ -1,8 +1,12 @@
 import seed from './seed'
+import CheckACL from '../server/models/CheckACL'
 import 'mocha'
 
 before(function() {
   this.timeout(0)
+  CheckACL.checkDb((err: Error) => {
+    if (err) throw err
+  })
   return seed.execute()
 })
 
