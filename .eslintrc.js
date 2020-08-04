836b6e780f85ff50b1e4b03a04b91973715994c2
Denis Rudnei de Souza <denisrudneidesouza@gmail.com>
2020-08-04 14:30:22 -0300

    Fixed eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    browser: true,
+    es2020: true,
     node: true,
   },
   extends: [
@@ -8,11 +8,14 @@ module.exports = {
     'airbnb-base',
   ],
   parserOptions: {
+    ecmaVersion: 11,
     parser: '@typescript-eslint/parser',
+    sourceType: 'module',
   },
   plugins: [
+    'vue',
     'vuetify',
-    '@typescript-eslint/eslint-plugin',
+    '@typescript-eslint',
   ],
   rules: {
     'import/extensions': 'off',
